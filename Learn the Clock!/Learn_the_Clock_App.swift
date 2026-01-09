1b9ee8bb1a0c382341d38b8c19df6a624cc79cd4
Sebastian Strus <sebastian.strus1@gmail.com>
2026-01-09 23:59:16 +0100

    Initial Commit

---
+++ Learn the Clock!/Learn_the_Clock_App.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_the_Clock_App.swift
+//  Learn the Clock!
+//
+//  Created by Sebastian Strus on 1/9/26.
+//
+
+import SwiftUI
+
+@main
+struct Learn_the_Clock_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
