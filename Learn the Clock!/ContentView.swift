ce0d92dd9616282fb0786e7e9e3de7410c2ed9cb
Sebastian Strus <sebastian.strus1@gmail.com>
2026-03-01 18:51:00 +0100

    Removed Triangle Shape

--- Learn the Clock!/ContentView.swift
+++ Learn the Clock!/ContentView.swift
@@ -633,18 +633,6 @@ struct CenterJewel: View {
     }
 }
 
-// MARK: - Triangle Shape (kept for compatibility)
-struct Triangle: Shape {
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.closeSubpath()
-        return path
-    }
-}
-
 
 struct FallingCoinsView: View {
     @State private var coins: [Coin] = []
